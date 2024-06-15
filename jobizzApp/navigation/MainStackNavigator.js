7a32a7847b811cb7a6fc557f38d1084f53748f22
Henry Quaye <125556097+henryquayeesq@users.noreply.github.com>
2024-06-15 20:15:25 +0100

    app

--- jobizzApp/navigation/MainStackNavigator.js
+++ jobizzApp/navigation/MainStackNavigator.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import LoginScreen from '../screens/loginScreen';
+import LoginScreen from '../screens/LoginScreen';
 import HomeScreen from '../screens/HomeScreen';
 
 const Stack = createStackNavigator();
